15c0945e84e70b586964cdfc49c251f6773d7af4
alaaalhrebi <alaa.alhrebi@gmail.com>
2026-07-05 13:22:05 +0300

    Update page.tsx

--- inteleye_project/src/app/page.tsx
+++ inteleye_project/src/app/page.tsx
@@ -3,6 +3,7 @@ import HeroSection from "@/components/HeroSection";
 import Stats from "@/components/Stats";
 import Features from "@/components/Features";
 import AISection from "@/components/AISection";
+import Pricing from "@/components/Pricing/Pricing";
 import Testimonials from "@/components/Testimonials";
 import Footer from "@/components/Footer";
 import AnimatedText from "@/components/AnimatedText";
@@ -15,6 +16,7 @@ export default function Home() {
       <Stats />
       <Features />
       <AISection />
+      <Pricing />
       <Testimonials />
       <Footer />
       <AnimatedText>Inteleye</AnimatedText>
