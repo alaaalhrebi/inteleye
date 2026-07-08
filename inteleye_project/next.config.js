a2ceea0739afecd4625c03f52b82b90482ebc8bf
alaaalhrebi <alaa.alhrebi@gmail.com>
2026-07-09 00:38:35 +0300

    Update next.config.js

--- inteleye_project/next.config.js
+++ inteleye_project/next.config.js
@@ -1,7 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+/*const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    unoptimized: true,
+  },
+};
+
+module.exports = nextConfig;
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
   images: {
     unoptimized: true,
   },
