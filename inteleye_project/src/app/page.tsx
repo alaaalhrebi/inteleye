328b2bcaec04a940fcb3deb52d20cf73dcc93940
alaaalhrebi <alaa.alhrebi@gmail.com>
2026-07-16 18:10:39 +0300

    Update page.tsx

--- inteleye_project/src/app/page.tsx
+++ inteleye_project/src/app/page.tsx
@@ -19,7 +19,7 @@ export default function Home() {
       <Pricing />
       <Testimonials />
       <Footer />
-<AnimatedText>IntelEye</AnimatedText>
+/*<AnimatedText>IntelEye</AnimatedText>*/
     </>
   );
 }
